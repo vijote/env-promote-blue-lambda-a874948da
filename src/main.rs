0e1abbc20abc237cbe330a32a0f00adde0331cc7
Juan Ignacio Dominguez <juan.ignacio.dominguez.-nd@disney.com>
2026-06-18 23:31:11 -0300

    fix(update-blue): remove usage in previous blue environments

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::env;
 
 use aws_config::BehaviorVersion;
 use aws_sdk_cloudfront::Client as CloudFrontClient;
-use lambda_http::{Body, Error, Request, Response, run, service_fn};
-use aws_sdk_dynamodb::{Client as DynamoClient};
 use aws_sdk_dynamodb::types::AttributeValue;
+use aws_sdk_dynamodb::Client as DynamoClient;
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use serde::Deserialize;
 use serde_json::json;
 
@@ -29,20 +29,24 @@ async fn main() -> Result<(), Error> {
     .await
 }
 
-async fn promote_staging_handler(req: Request, cloudfront_client: &CloudFrontClient, dynamo_client: &DynamoClient,) -> Result<Response<Body>, Error> {
+async fn promote_staging_handler(
+    req: Request,
+    cloudfront_client: &CloudFrontClient,
+    dynamo_client: &DynamoClient,
+) -> Result<Response<Body>, Error> {
     // 1. Parsear los datos de entrada del HTTP Request
     let body_bytes = req.body().as_ref();
     let table_name = env::var("TABLE_NAME").expect("TABLE_NAME not set!!");
     let payload: PromoteRequest = match serde_json::from_slice(body_bytes) {
         Ok(p) => p,
         Err(_) => {
-            return Ok(Response::builder()
-                .status(400)
-                .body(Body::from(json!({ "error": "Invalid JSON body" }).to_string()))?)
+            return Ok(Response::builder().status(400).body(Body::from(
+                json!({ "error": "Invalid JSON body" }).to_string(),
+            ))?)
         }
     };
 
-    // 2. Para actualizar la distribución primaria con el staging modifier, 
+    // 2. Para actualizar la distribución primaria con el staging modifier,
     // AWS CloudFront requiere obligatoriamente el ETag de la distribución Primaria (Producción)
     let primary_config_output = match cloudfront_client
         .get_distribution_config()
@@ -52,45 +56,50 @@ async fn promote_staging_handler(req: Request, cloudfront_client: &CloudFrontCli
     {
         Ok(output) => output,
         Err(err) => {
-            return Ok(Response::builder()
-                .status(500)
-                .body(Body::from(json!({ "error": format!("Error obteniendo config primaria: {:?}", err) }).to_string()))?)
+            return Ok(Response::builder().status(500).body(Body::from(
+                json!({ "error": format!("Error obteniendo config primaria: {:?}", err) })
+                    .to_string(),
+            ))?)
         }
     };
 
     let staging_config_output = cloudfront_client
-    .get_distribution_config()
-    .id(&payload.staging_distribution_id)
-    .send()
-    .await?;
+        .get_distribution_config()
+        .id(&payload.staging_distribution_id)
+        .send()
+        .await?;
 
-    let primary_etag = primary_config_output
-        .e_tag()
-        .expect("primary etag missing");
+    let primary_etag = primary_config_output.e_tag().expect("primary etag missing");
 
-    let staging_etag = staging_config_output
-        .e_tag()
-        .expect("staging etag missing");
+    let staging_etag = staging_config_output.e_tag().expect("staging etag missing");
 
     let if_match = format!("{}, {}", primary_etag, staging_etag);
 
     if let Err(err) = dynamo_client
         .update_item()
         .table_name(&table_name)
-        .key("environment", AttributeValue::S(payload.environment_id.to_string()))
-        .update_expression("SET env_status = :newStatus")
-        .expression_attribute_values(
-            ":newStatus",
-            AttributeValue::S("blue".to_string()),
+        .key(
+            "environment",
+            AttributeValue::S(payload.environment_id.to_string()),
         )
+        .update_expression("SET env_status = :newStatus")
+        .expression_attribute_values(":newStatus", AttributeValue::S("blue".to_string()))
         .send()
         .await
     {
+        return Ok(Response::builder().status(500).body(Body::from(
+            json!({ "error": format!("Error durante la promoción: {:?}", err) }).to_string(),
+        ))?);
+    };
+
+    if let Err(err) = update_existing_blue(&dynamo_client, &table_name).await {
         return Ok(Response::builder()
             .status(500)
-            .body(Body::from(json!({ "error": format!("Error durante la promoción: {:?}", err) }).to_string()))?)
-        
-    };
+            .body(Body::from(
+                json!({ "error": format!("Error updating blue env! {:?}", err) }).to_string(),
+            ))
+            .expect("Error setting update blue env response body!"));
+    }
 
     // 3. Ejecutar la promoción atómica
     // Esto copia los Origins de la staging distribution directamente a la producción estándar.
@@ -115,10 +124,55 @@ async fn promote_staging_handler(req: Request, cloudfront_client: &CloudFrontCli
                     )
                 }).to_string()))?)
         }
-        Err(err) => {
-            Ok(Response::builder()
-                .status(500)
-                .body(Body::from(json!({ "error": format!("Error durante la promoción: {:?}", err) }).to_string()))?)
-        }
+        Err(err) => Ok(Response::builder().status(500).body(Body::from(
+            json!({ "error": format!("Error durante la promoción: {:?}", err) }).to_string(),
+        ))?),
     }
+}
+
+async fn update_existing_blue(
+    dynamo_client: &DynamoClient,
+    table_name: &str,
+) -> Result<(), String> {
+    let results = dynamo_client
+        .scan()
+        .table_name(table_name)
+        .filter_expression("env_status = :status_val")
+        .expression_attribute_values(":status_val", AttributeValue::S("blue".to_string()))
+        // Opcional: Proyecta solo los atributos que necesitas para ahorrar ancho de banda
+        .projection_expression("environment")
+        .send()
+        .await
+        .expect("Error finding blue environments!");
+
+    let blue_env = results
+        .items
+        .as_ref()
+        .and_then(|items| items.first())
+        .and_then(|item| item.get("environment"))
+        .and_then(|attr| match attr {
+            AttributeValue::S(s) => Some(s.clone()),
+            _ => None,
+        });
+
+    if let None = blue_env {
+        return Ok(());
+    }
+
+    if let Err(err) = dynamo_client
+        .update_item()
+        .table_name(table_name)
+        .key(
+            "environment",
+            AttributeValue::S(blue_env.expect("Error unwraping blue env id!").to_string()),
+        )
+        .update_expression("SET env_status = :newStatus")
+        .expression_attribute_values(":newStatus", AttributeValue::S("unused".to_string()))
+        .send()
+        .await
+    {
+        return Err(format!("Error updating blue environment! {}", err).to_string());
+    };
+
+    return Ok(());
 }
